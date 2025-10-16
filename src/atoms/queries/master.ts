c9af222111c2f7615e45881e1f60ec2e8f64aa08
koo-arch <yukshw.ko.0623@gmail.com>
2025-10-16 13:50:38 +0000

    refactor: Move master data fetching to server components

--- src/atoms/queries/master.ts
+++ src/atoms/queries/master.ts
@@ -1,53 +1,19 @@
-import axios from '@/lib/axios/public';
-import { atomWithQuery } from 'jotai-tanstack-query';
+import { atom } from 'jotai';
 import { Class, Attribute, OrderAlignment, MoralAlignment, Trait } from '@/types/master';
 
-export const classAtom = atomWithQuery(
-    () => ({
-        queryKey: ['classes'],
-        queryFn: async () => {
-            const { data } = await axios.get<Class[]>('master/classes');
-            return data;
-        }
-    })
-);
+export type Masters = {
+    class: Class[];
+    trait: Trait[];
+    attribute: Attribute[];
+    orderAlignment: OrderAlignment[];
+    moralAlignment: MoralAlignment[];
+};
 
-export const attributeAtom = atomWithQuery(
-    () => ({
-        queryKey: ['attributes'],
-        queryFn: async () => {
-            const { data } = await axios.get<Attribute[]>('master/attributes');
-            return data;
-        }
-    })
-);
-
-export const orderAlignmentAtom = atomWithQuery(
-    () => ({
-        queryKey: ['orderAlignments'],
-        queryFn: async () => {
-            const { data } = await axios.get<OrderAlignment[]>('master/order-alignments');
-            return data;
-        }
-    })
-);
-
-export const moralAlignmentAtom = atomWithQuery(
-    () => ({
-        queryKey: ['moralAlignments'],
-        queryFn: async () => {
-            const { data } = await axios.get<MoralAlignment[]>('master/moral-alignments');
-            return data;
-        }
-    })
-);
-
-export const traitAtom = atomWithQuery(
-    () => ({
-        queryKey: ['traits'],
-        queryFn: async () => {
-            const { data } = await axios.get<Trait[]>('master/traits');
-            return data;
-        }
-    })
-);
+// Hydratorで初期値注入
+export const mastersAtom = atom<Masters>({
+    class: [],
+    trait: [],
+    attribute: [],
+    orderAlignment: [],
+    moralAlignment: [],
+});
